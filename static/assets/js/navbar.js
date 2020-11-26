c280213bbfea1a9bd8fb382302dae10a7eb605f5
Patrick Magauran <patmagauran.j@gmail.com>
2020-11-26 14:25:25 -0500

    Changes Navbar to always have bg.

--- static/assets/js/navbar.js
+++ static/assets/js/navbar.js
@@ -7,9 +7,8 @@
     // ==================================================
     // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
     // $.onscroll = function() {scrollFunction()};
-    $(document).scroll(function () {
-      if ($(document).scrollTop() > 40) {
-        $('#top-navbar').removeClass('initial-navbar');
+
+    $('#top-navbar').removeClass('initial-navbar');
         $('#top-navbar').addClass('final-navbar shadow');
 
         $('#navbar-toggler').removeClass('navbar-dark');
@@ -18,18 +17,30 @@
         // get the main logo from hidden img tag
         let mainLogo = document.getElementById("main-logo").getAttribute("src");
         $('#logo').attr("src", mainLogo);
-      } else {
-        $('#top-navbar').removeClass('final-navbar shadow');
-        $('#top-navbar').addClass('initial-navbar');
 
-        $('#navbar-toggler').removeClass('navbar-light');
-        $('#navbar-toggler').addClass('navbar-dark');
+    // $(document).scroll(function () {
+    //   if ($(document).scrollTop() > 40) {
+    //     $('#top-navbar').removeClass('initial-navbar');
+    //     $('#top-navbar').addClass('final-navbar shadow');
 
-        // get the inverted logo from hidden img tag
-        let invertedLogo = document.getElementById("inverted-logo").getAttribute("src");
-        $('#logo').attr("src", invertedLogo);
-      }
-    });
+    //     $('#navbar-toggler').removeClass('navbar-dark');
+    //     $('#navbar-toggler').addClass('navbar-light');
+
+    //     // get the main logo from hidden img tag
+    //     let mainLogo = document.getElementById("main-logo").getAttribute("src");
+    //     $('#logo').attr("src", mainLogo);
+    //   } else {
+    //     $('#top-navbar').removeClass('final-navbar shadow');
+    //     $('#top-navbar').addClass('initial-navbar');
+
+    //     $('#navbar-toggler').removeClass('navbar-light');
+    //     $('#navbar-toggler').addClass('navbar-dark');
+
+    //     // get the inverted logo from hidden img tag
+    //     let invertedLogo = document.getElementById("inverted-logo").getAttribute("src");
+    //     $('#logo').attr("src", invertedLogo);
+    //   }
+    // });
 
     // Creates a click handler to collapse the navigation when
     // anchors in the mobile nav pop up are clicked
